37ff1e84e5d1b23b96c8fce7a2a41e72d3758f7d
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2017-12-01 14:10:00 -0200

    Adding message translation

---
+++ src/jsonpatch.d.ts
@@ -0,0 +1,23 @@
+export function apply_patch<D>(doc: D, patch: string | Array<any>): D;
+
+export interface InvalidPatch extends Error {
+  new (message: string): InvalidPatch;
+}
+
+export interface PatchApplyError extends Error {
+  new (message: string): PatchApplyError;
+}
+
+export interface JSONPointer {
+  new (pathStr: string): JSONPointer;
+  add<D>(doc: D, value: any, mutate?: boolean, optional?: boolean): D;
+  remove<D>(doc: D, mutate?: boolean, optional?: boolean): D;
+  replace<D>(doc: D, value: any, mutate?: boolean, optional?: boolean): D;
+  get<D>(doc: D, optional?: boolean): any;
+  subsetOf(otherPointer: JSONPointer): boolean;
+}
+
+export interface JSONPatch {
+  new (patch: string | Array<any>, mutate?: boolean): JSONPatch;
+  apply<D>(doc: D): D;
+}
