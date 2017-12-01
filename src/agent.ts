2a9760df1e781d33ed475c77a16f8a31cdb2a66d
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2017-12-01 13:52:47 -0200

    Implementing kafka support for device manager notifications

---
+++ src/agent.ts
@@ -0,0 +1,104 @@
+import fs = require("fs");
+import util = require("util");
+import mqtt = require("mqtt");
+import mqttHandler = require("./mqtt-handler");
+import { ConfigOptions } from "./config";
+import { tokenize } from "./tools";
+import { DataBroker } from "./data-broker";
+import { IdResolver, CacheHandler } from "./cache";
+import { resolve } from "url";
+
+/**
+ * IoT agent class
+ * This class is responsible for orchestrating MQTT handler and
+ * Device manager handler, sending Orion update messages as
+ * needed.
+ */
+class Agent {
+  // Main configuration structure.
+  configuration: ConfigOptions;
+
+  // MQTT Context used by this agent.
+  // This is only necessary when stopping MQTT.
+  mqttContext: mqtt.Client;
+
+  // Broker which will receive device update messages.
+  dataBroker: DataBroker;
+
+  // Cache which will hold everything (almost) received via kafka.
+  cacheHandler: CacheHandler;
+  
+  // Tool to find out the device ID from a received message.
+  idResolver: IdResolver
+
+  constructor(config: ConfigOptions, dataBroker: DataBroker, cache: CacheHandler, resolver: IdResolver) {
+    this.configuration = config;
+    this.dataBroker = dataBroker;
+    this.cacheHandler = cache;
+    this.idResolver = resolver;
+  };
+
+  /**
+   * Process a message received via MQTT
+   * @param {string} topic The topic through which the message was published
+   * @param {string} message The received message
+   */
+  processMessage(topic: string, message: string): void {
+    console.log('Got new MQTT message');
+    console.log('Topic: ' + topic);
+    console.log('Content:' + message);
+    let messageObj = JSON.parse(message);
+
+    // The message 'format' can be detected by its topic.
+    // TODO: the user might choose to use this 'message topic format switch'
+    // as a "/device/+/deviceinfo" 
+    let id = this.idResolver.resolve(topic, messageObj, { "topic" : topic });
+    if (id === "") {
+      console.log("No device ID was detected. Skipping this message.")
+      return;
+    }
+    console.log("Detected device ID: " + id);
+
+    let deviceData = this.cacheHandler.lookup(id);
+    let filteredObj: any = {};
+    // Message matches default message structure
+    for (let attr in messageObj) {
+      for (let template in deviceData.data.attrs) {
+        for (let cfgAttr of deviceData.data.attrs[template]) {
+          if (cfgAttr.label === attr) {
+            filteredObj[attr] = messageObj[attr];
+            // Found it. Next attribute
+            break;
+          }
+        }
+      }
+    }
+
+    console.log("Sending device update: ");
+    console.log("Device ID: " + id);
+    console.log("Service: " + deviceData.meta.service);
+    console.log("Data: ");
+    console.log(util.inspect(filteredObj, {depth: null}));
+    this.dataBroker.updateData(deviceData.meta.service, id, filteredObj);
+  }
+
+  /**
+   * Start MQTT message reception.
+   */
+  startMqtt() {
+    this.mqttContext = mqttHandler.start(this.configuration, 
+      (topic: string, message: string) => { this.processMessage(topic, message); }, 
+      (error: Error) => { console.log("Error with MQTT operation: " + error); });
+  }
+
+  /**
+   * Stop MQTT message reception.
+   */
+  stopMqtt() {
+    mqttHandler.stop(this.mqttContext, (error: Error) => {
+      console.log("Error with MQTT operation: " + error);
+    });
+  }
+}
+
+export {Agent};
---
+++ src/cache.ts
@@ -0,0 +1,227 @@
+import util = require("util");
+
+
+interface DeviceManagerEventData {
+  id: string;
+  [attrs: string] : any;
+}
+
+interface DeviceManagerEvent {
+  event: "created" | "removed" | "updated";
+  data: DeviceManagerEventData;
+  meta: any;
+}
+
+/**
+ * Instructions on how to extract device ID
+ * from a received message.
+ */
+interface ResolverInstructions {
+  // Where this ID is
+  type: "mqtt-topic" | "message-attribute"
+
+  // If 'message-attribute' is selected,
+  // this selected which attribute contains the ID
+  attribute_name?: string
+
+  // If necessary, this regex will be used
+  // to extract the ID. This is valid for both
+  // mqtt-topic and message-attribute.
+  regexp?: string
+
+  // Physical device ID
+  id: string 
+
+  // Any message property besides payload that could indicate message format.
+  // This could be, for instance, MQTT topic.
+  xid: string
+}
+
+
+/**
+ * Class responsible for device cache management and cache lookup
+ */
+class CacheHandler {
+
+  // The cache.
+  // TODO: this would be better placed in a Redis instance.
+  cache: {
+    [id: string]: any
+  }
+
+  constructor() {
+    this.cache = {};
+  }
+
+  /**
+   * Process a new event got from DeviceManager.
+   * @param event The received event
+   */
+  processEvent(event: DeviceManagerEvent) {
+    console.log("Received event: " + util.inspect(event, { depth: null}));
+
+    switch (event.event) {
+      case "created":
+      case "updated":
+      console.log("Inserting into cache new device description.");
+        this.cache[event.data.id] = event; 
+      break;
+      case "removed":
+      console.log("Removing from cache device description.");
+        delete this.cache[event.data.id]
+      break;
+    }
+
+    console.log("Cache so far: ");
+    console.log(util.inspect(this.cache, {depth: null}));
+  }
+
+  /**
+   * Perform a cache lookup.
+   * @param id Device ID
+   * @returns The cached device data
+   */
+  lookup(id: string) : any {
+    if (id in this.cache) {
+      return this.cache[id];
+    } else {
+      return null;
+    }
+  }
+};
+
+/**
+ * Class responsible for device ID resolution
+ * from a received message.
+ */
+class IdResolver {
+  // The cache. 
+  cache: {
+    [id: string]: ResolverInstructions
+  }
+
+  // Map correlating dojot's device ID and physical
+  // device ID
+  idCache: {
+    [id: string]: string
+  }
+
+  constructor() {
+    this.cache = {};
+    this.idCache = {};
+  }
+
+  /**
+   * Extract all instructions to locate device ID.
+   * 
+   * @param event The received device manager event
+   * @returns A list of instructions to locate physical device ID from
+   * a received message.
+   */
+  extractIdLocations(event: DeviceManagerEvent) : ResolverInstructions[] {
+    let ret: ResolverInstructions[] = [];
+
+    for (let templateId in event.data.attrs) {
+      for (let templateAttr of event.data.attrs[templateId]) {
+        if (templateAttr["label"] === "id-location") {
+          ret.push(JSON.parse(templateAttr["static_value"]));
+        }
+      }
+    }
+
+    return ret;
+  }
+
+  /**
+   * Process a device manager event.
+   * 
+   * All device ID location instructions will be gathered and 
+   * saved to the cache.
+   * 
+   * @param event The received event.
+   */
+  processEvent(event: DeviceManagerEvent) {
+    let idLocations = this.extractIdLocations(event);
+
+    if (idLocations.length == 0) {
+      console.log("No ID location was found.")
+      return;
+    }
+
+    switch (event.event) {
+      case "created":
+      case "updated":
+        console.log("Inserting into resolver new ID resolution instruction.");
+        for (let instruction of idLocations) {
+          console.log("Instruction: " + util.inspect(instruction, {depth: null}));
+          this.cache[instruction.xid] = instruction;
+          this.idCache[instruction.id] = event.data.id;
+        }
+      break;
+      case "removed":
+        console.log("Removing from resolver an ID resolution instruction.");
+        for (let instruction of idLocations) {
+          delete this.cache[instruction.xid];
+          delete this.idCache[instruction.id];
+        }
+      break;
+    }
+
+    console.log("Cache so far: ");
+    console.log(util.inspect(this.cache, {depth: null}));
+    console.log("ID Cache so far: ");
+    console.log(util.inspect(this.idCache, {depth: null}));
+  }
+
+  /**
+   * Resolve dojot's device ID from data sent from the
+   * physical device.
+   * @param xid Message property value that indicates message format.
+   * @param data The received message
+   * @param meta Meta-information related to the message. The
+   * only attribute used is 'topic', which holds the topic
+   * to which the message was published.
+   * @returns The associated dojot's device ID
+   */
+  resolve(xid: string, data?: any, meta?: any) : string {
+    let ret = "";
+
+    if (!(xid in this.cache)) {
+      console.log("ID " + xid + " was not found in resolver cache.");
+      return "";
+    }
+
+    let instruction = this.cache[xid];
+    let dataSource: string;
+    switch (instruction.type) {
+      case "mqtt-topic":
+         dataSource = meta["topic"];
+      break;
+      case "message-attribute":
+        // TODO: This attribute should be parsed and analyzed in-depth
+        if (data !== undefined && instruction.attribute_name !== undefined) { 
+          dataSource = data[instruction.attribute_name]; 
+        }
+        else { dataSource = ""; }
+      break;
+      default:
+        dataSource = "";
+    }
+
+    if (instruction.regexp !== undefined) {
+      ret = dataSource.replace(new RegExp(instruction.regexp, "g"), "$1");
+    } else {
+      ret = dataSource;
+    }
+
+    if (ret != "") {
+      // Transform to dojot's ID
+      ret = this.idCache[ret];
+    }
+    return ret;
+  }
+};
+
+export { IdResolver }
+export { CacheHandler }
+export { DeviceManagerEvent }
