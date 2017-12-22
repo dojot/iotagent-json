c66016db74195833f1e8851b88666103eca149c1
Alexandre Vasconcellos <alexv@cpqd.com.br>
2017-12-22 16:22:26 -0200

    fixed a typo on MQTT_TLS env variable check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /opt/iotagent-json/
 
-if [ "$MQTT_TLS" == "true"] ; then
+if [ "$MQTT_TLS" = "true" ] ; then
   python initialConf.py
   if [ $? -ne 0 ]; then
       echo "Error ocurred on initial iotagent TLS setup"
