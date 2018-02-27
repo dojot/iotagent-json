25515e0f3c38b80d8b5d4129d78c8e72822a8b46
Manuel Gavidia <manuelg@cpqd.com.br>
2018-02-27 16:11:25 -0300

    Remove description file moved to lib repo

--- src/dojot-clientlib.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'dojot-clientlib' {
-  export class AlarmConn {
-    constructor ( hostname? : string, port? : string, username? : string, password? : string);
-    close() : void;
-    send(alarm : any) : void;
-  }
-}
